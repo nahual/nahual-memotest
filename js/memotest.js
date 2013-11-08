76584686544aac307a88540c1710cb0354e726a6
juan <jplagostena@gmail.com>
2013-11-07 23:28:32 -0300

    Avances en layout + comportamiento. Falta implementar reglas del memotest

--- js/memotest.js
+++ js/memotest.js
@@ -1,17 +1,42 @@
 const MAX_IMAGENES = 10;
+const FICHA_OCULTA_SRC="img/Nahual.jpg";
 
+
+function Ficha(idxImagen, posicion, tupla) {
+    this.imagen = "img/" + (idxImagen + 1) + ".JPG";
+    this.posicion = posicion;
+    this.isFichaOculta = true;
+    this.myDiv = null;
+    this.tupla = tupla;
+}
+
+Ficha.prototype.paint = function($parent)
+{
+    var $divFicha = $("<div class='ficha'><img src='" + FICHA_OCULTA_SRC + "'></div>");
+    this.myDiv = $divFicha;
+    var self = this;
+    $divFicha.click(function() {
+        self.onclick();
+    });
+    $parent.append($divFicha);
+
+};
+
+Ficha.prototype.onclick = function() {
+    debugger;
+    if (this.isFichaOculta) {
+        this.myDiv.find("img").attr("src", this.imagen);
+    } else {
+        this.myDiv.find("img").attr("src", FICHA_OCULTA_SRC);
+    }
+    this.isFichaOculta= !this.isFichaOculta;
+}
 
 function Tupla(idxImagen, pos1, pos2) {
     //una tupla va a tener dos posiciones en el tablero con la misma imágen
-    this.posicion1=pos1;
-    this.posicion2=pos2;
-    this.imagen= "../img/" + idxImagen + ".JPG";
+    this.ficha1= new Ficha(idxImagen, pos1, this);
+    this.ficha2= new Ficha(idxImagen, pos2, this);
 }
-
-Tupla.prototype.paint = function()
-{
-
-};
 
 function Tablero() {
 
@@ -22,7 +47,6 @@ function Tablero() {
     for (var i=0; i < MAX_IMAGENES; i++) {
         var pos1 = getPosicionDisponible();
         var pos2 = getPosicionDisponible();
-        console.log(pos1 + " | " + pos2 + " | tupla: " + i);
         this.tuplas.push(new Tupla(i, pos1, pos2));
     }
 
@@ -51,10 +75,17 @@ function Tablero() {
 
 Tablero.prototype.paint = function()
 {
-
+    var fichasOrdenadas = [];
     for (var i=0; i < this.tuplas.length; i++ ) {
-        console.log("Posicion 1: " + this.tuplas[i].posicion1);
-        console.log("Posicion 2: " + this.tuplas[i].posicion2);
+        fichasOrdenadas[this.tuplas[i].ficha1.posicion] = this.tuplas[i].ficha1;
+        fichasOrdenadas[this.tuplas[i].ficha2.posicion] = this.tuplas[i].ficha2;
+    }
+
+    for (var i = 0; i< fichasOrdenadas.length; i++ ) {
+        var $memotestContainer = $("#fichasContainer");
+        //imprimo filas
+        var $filaParent = $memotestContainer.find("#fila" + ((i % 4)));
+        fichasOrdenadas[i].paint($filaParent);
     }
 };
 
